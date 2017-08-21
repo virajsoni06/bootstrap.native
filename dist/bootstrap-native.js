ed4ab32a96ef7f969677dd9336bc38d533f81ffe
Viraj Soni <viraj@bearddesign.co>
2017-08-21 12:18:06 +0530

    including tabs

--- dist/bootstrap-native.js
+++ dist/bootstrap-native.js
@@ -1,4 +1,4 @@
-// Native Javascript for Bootstrap 3 v2.0.12 | © dnp_theme | MIT-License
+// Native Javascript for Bootstrap 3 v2.0.14 | © dnp_theme | MIT-License
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD support:
@@ -10,6 +10,7 @@
     // Browser globals (root is window)
     var bsn = factory();
     root.Modal = bsn.Modal;
+    root.Tab = bsn.Tab;
   }
 }(this, function () {
   
@@ -530,7 +531,141 @@
   initializeDataAPI(stringModal, Modal, dataToggle);
   
   
+  /* Native Javascript for Bootstrap 3 | Tab
+  -----------------------------------------*/
+  
+  // TAB DEFINITION
+  // ==============
+  var Tab = function( element, options ) {
+  
+    // initialization element
+    element = queryElement(element);
+  
+    // DATA API
+    var heightData = element[getAttribute](dataHeight),
+      
+        // strings
+        component = 'tab', height = 'height', isAnimating = 'isAnimating';
+  
+    // set default animation state
+    element[isAnimating] = false;
+  
+    // set options
+    options = options || {};
+    this[height] = supportTransitions ? (options[height] || heightData === 'true') : false; // filter legacy browsers
+  
+    // bind, event targets
+    var self = this, next,
+      tabs = getClosest(element,'.nav'),
+      tabsContentContainer,
+      dropdown = tabs && queryElement('.dropdown',tabs),
+      activeTab, activeContent, nextContent,
+      // trigger
+      triggerShow = function() {
+        bootstrapCustomEvent.call(next, shownEvent, component, activeTab);
+        if (tabsContentContainer) { // height animation
+          (function(){
+            setTimeout(function(){
+              tabsContentContainer[style][height] = '';
+              removeClass(tabsContentContainer,collapsing);
+              activeTab[isAnimating] = next[isAnimating] = false;
+            },200);
+          }());
+        } else { 
+          activeTab[isAnimating] = next[isAnimating] = false; 
+        }
+      },
+      triggerHide = function() {
+        removeClass(activeContent,active);
+        addClass(nextContent,active);
+        setTimeout(function() {
+          addClass(nextContent,inClass);
+          nextContent[offsetHeight];
+          if (tabsContentContainer) addClass(tabsContentContainer,collapsing);
+          (function() {
+            bootstrapCustomEvent.call(next, showEvent, component, activeTab);
+            (function() {
+              if(tabsContentContainer) tabsContentContainer[style][height] = getMaxHeight(nextContent) + 'px'; // height animation
+              bootstrapCustomEvent.call(activeTab, hiddenEvent, component, next);
+            }());
+          }());
+        },20);
+      };
+  
+    if (!tabs) return; // invalidate 
+  
+    // private methods
+    var getActiveTab = function() {
+        var activeTabs = getElementsByClassName(tabs,active), activeTab;
+        if ( activeTabs[length] === 1 && !hasClass(activeTabs[0],'dropdown') ) {
+          activeTab = activeTabs[0];
+        } else if ( activeTabs[length] > 1 ) {
+          activeTab = activeTabs[activeTabs[length]-1];
+        }
+        return activeTab[getElementsByTagName]('A')[0];
+      },
+      getActiveContent = function() {
+        return queryElement(getActiveTab()[getAttribute]('href'));
+      },
+      // handler
+      clickHandler = function(e) {
+        e.preventDefault();
+        next = e[target][getAttribute](dataToggle) === component || targetsReg.test(e[target][getAttribute]('href')) 
+             ? e[target] : e[target][parentNode]; // allow for child elements like icons to use the handler
+        self.show();
+      };
+  
+    // public method
+    this.show = function() { // the tab we clicked is now the next tab
+      next = next || element;
+      nextContent = queryElement(next[getAttribute]('href')); //this is the actual object, the next tab content to activate
+      activeTab = getActiveTab(); 
+      activeContent = getActiveContent();
+  
+      if ( (!activeTab[isAnimating] || !next[isAnimating]) && !hasClass(next[parentNode],active) ) {
+        activeTab[isAnimating] = next[isAnimating] = true;
+        removeClass(activeTab[parentNode],active);
+        addClass(next[parentNode],active);
+  
+        if ( dropdown ) {
+          if ( !hasClass(element[parentNode][parentNode],'dropdown-menu') ) {
+            if (hasClass(dropdown,active)) removeClass(dropdown,active);
+          } else {
+            if (!hasClass(dropdown,active)) addClass(dropdown,active);
+          }
+        }
+        
+        if (tabsContentContainer) tabsContentContainer[style][height] = getMaxHeight(activeContent) + 'px'; // height animation
+  
+        (function(){
+          removeClass(activeContent,inClass);
+          bootstrapCustomEvent.call(activeTab, hideEvent, component, next);
+          (function(){
+            hasClass(activeContent, 'fade') ? emulateTransitionEnd(activeContent, triggerHide) : triggerHide();
+          }());
+        }());
+  
+        (function(){
+          hasClass(nextContent, 'fade') ? emulateTransitionEnd(nextContent, triggerShow) : triggerShow();
+        }());
+      }
+    };
+  
+    // init
+    if ( !(stringTab in element) ) { // prevent adding event handlers twice
+      on(element, clickEvent, clickHandler);
+    }
+    if (this[height]) { tabsContentContainer = getActiveContent()[parentNode]; }
+    element[stringTab] = this;
+  };
+  
+  // TAB DATA API
+  // ============
+  initializeDataAPI(stringTab, Tab, dataToggle);
+  
+  
   return {
-    Modal: Modal
+    Modal: Modal,
+    Tab: Tab
   };
 }));
